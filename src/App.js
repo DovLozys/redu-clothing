de3c84aca431e7c81449f71da6cc4e1c558d6f7b
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-08-16 18:27:42 +0100

    adds categories list, renders basic ui out of it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 const App = () => {
-  return <div>Hello World.</div>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
+  return (
+    <div className='container-categories'>
+      {categories.map(({title}) => (
+        <div className='container-category'>
+          <div className='background-image'></div>
+          <div className='container-category-body'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
