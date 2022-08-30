f9fc2d838fc6191e9cdd0d0d0060229b519daa29
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-08-30 21:15:51 +0100

    quotes and spaces...

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,18 @@
-import { signInWithGooglePopup, createUserDocument } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createUserDocument
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocument(user);
   }
 
   return (
     <div>
       <h1>Sign in page.</h1>
-      <button onClick={logGoogleUser}>sign in</button>
+      <button onClick={logGoogleUser}>Pop-up sign in</button>
     </div>
   );
 }
